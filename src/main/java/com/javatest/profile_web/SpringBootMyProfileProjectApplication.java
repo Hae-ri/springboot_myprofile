f215b746479ed218f656f246e839b9e231c53f2f
ici <ici@DESKTOP-JJRURRS>
2022-05-19 15:12:09 +0900

    오라클클라우드.war

--- src/main/java/com/javatest/profile_web/SpringBootMyProfileProjectApplication.java
+++ src/main/java/com/javatest/profile_web/SpringBootMyProfileProjectApplication.java
@@ -2,12 +2,22 @@ package com.javatest.profile_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootMyProfileProjectApplication {
+public class SpringBootMyProfileProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMyProfileProjectApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SpringBootMyProfileProjectApplication.class);
+	}
+	
+	
 
 }
